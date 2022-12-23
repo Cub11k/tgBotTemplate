914da234097a450f16ff2abbb6f9d99adc4e6726
Cub11k <ostashenkokd@gmail.com>
2022-12-23 16:22:30 +0200

    Create bot/handlers/__init__.py

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,16 @@
+from telebot import TeleBot
+
+from bot.config import Config
+from bot.storages import Storage
+from bot.logger import Logger
+
+from bot.handlers.admin import AdminHandlers
+
+
+def register_handlers(bot: TeleBot, cfg: Config, storage: Storage, logger: Logger):
+    AdminHandlers(bot, cfg, storage, logger).register()
+
+
+__all__ = (
+    "register_handlers",
+)
