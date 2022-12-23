ebc7a83f7d861131ca50cbe10e7f8c153e5440b5
Cub11k <ostashenkokd@gmail.com>
2022-12-23 16:21:24 +0200

    Create bot/storages/pickle_storage.py

---
+++ bot/storages/pickle_storage.py
@@ -0,0 +1,31 @@
+import os
+import pickle
+import atexit
+from typing import Optional
+from bot.storages.base_storage import BaseStorage
+
+
+def create_dir(file_path):
+    dirs = file_path.rsplit('/', maxsplit=1)[0]
+    os.makedirs(dirs, exist_ok=True)
+    if not os.path.isfile(file_path):
+        with open(file_path, 'wb') as file:
+            pickle.dump({}, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+class PickleStorage(BaseStorage):
+    def __init__(self, file_path: Optional[str]):
+        super().__init__()
+        self.file_path = file_path if file_path is not None else "./.storages/data.pkl"
+        create_dir(self.file_path)
+        self.load()
+
+        atexit.register(self.dump)
+
+    def load(self):
+        with open(self.file_path, 'rb') as file:
+            self.data = pickle.load(file)
+
+    def dump(self):
+        with open(self.file_path, 'wb') as file:
+            pickle.dump(self.data, file, protocol=pickle.HIGHEST_PROTOCOL)
