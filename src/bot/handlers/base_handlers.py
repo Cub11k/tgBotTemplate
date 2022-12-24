5f4ded1c03b91213776c4218bd547bec4248fe73
Cub11k <ostashenkokd@gmail.com>
2022-12-25 00:20:28 +0200

    Add abc.ABC and abstractmethod

--- src/bot/handlers/base_handlers.py
+++ src/bot/handlers/base_handlers.py
@@ -1,6 +1,11 @@
-class BaseHandlers:
+import abc
+from abc import abstractmethod
+
+
+class BaseHandlers(abc.ABC):
     def __init__(self):
         pass
 
+    @abstractmethod
     def register(self):
         raise NotImplementedError
